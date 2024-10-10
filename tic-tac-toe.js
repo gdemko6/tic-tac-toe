0379306e74a8a1a4e8c203da5622fc0613e7d89b
Greg Demko <gregory.demko@my.uwrf.edu>
2024-10-10 15:19:01 -0500

    Add module for Gameboard

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+const Gameboard = (function () {
+
+    //create gameboard and create three rows of 3 columns
+    let gameboard = [];
+    for (let i = 0; i < 3; i++) {
+        gameboard[i] = ["", "", ""]; 
+    }
+})();
